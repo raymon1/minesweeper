202e4991dea2147e364321a87ab57d95763effb1
raymon <raymonmina@gmail.com>
2021-01-04 04:19:52 +0200

    [FEAT] uses hook instead of class for touch management

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import ContextMenuHandler from "./ContextMenuHandler";
+import useLongPress from "../hooks/useLongPress";
 
 export function Cell(props) {
     const cell = props.data.isCovered ? 'cell' : 'uncovered-cell';
     const number = !props.data.isCovered && !props.isMine ? `cell-${props.data.neighboringMines}` : '';
     const mine = !props.data.isCovered && props.isMine ? 'cell-mine' : '';
-    const content = props.data.isCovered ? 
-        (props.data.flagged ? '🚩' : '') : (props.data.isMine ? '💣' : props.data.neighboringMines > 0 ? props.data.neighboringMines : ''); 
+    const content = props.data.isCovered ?
+        (props.data.flagged ? '🚩' : '') : (props.data.isMine ? '💣' : props.data.neighboringMines > 0 ? props.data.neighboringMines : '');
     const clickedMine = props.data.clickedMine ? 'clicked-mine' : ''
 
-    const ctxMenuHandler = new ContextMenuHandler(_ => props.handleRightClick(props.data));
-
+    const callbacks = useLongPress(
+        () => props.handleRightClick(props.data),
+        () => props.handleLeftClick(props.data)
+    )
+    
     return (
-        <button 
-            className={`${cell} ${number} ${mine} ${clickedMine}`}
-            onClick={() => props.handleLeftClick(props.data)}
-            onContextMenu={ctxMenuHandler.onContextMenu}
-            onTouchStart={ctxMenuHandler.onTouchStart}
-            onTouchCancel={ctxMenuHandler.onTouchCancel}
-            onTouchEnd={ctxMenuHandler.onTouchEnd}
-            onTouchMove={ctxMenuHandler.onTouchMove}>
-                { content }
+        <button className={`${cell} ${number} ${mine} ${clickedMine}`}
+            {...callbacks}>
+            
+            { content}
+
         </button>
     );
 }
